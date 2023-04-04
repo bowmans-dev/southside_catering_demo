c2a5215e6bbd0d1c10cead8cf82509ee9f7abbc3
bowmans.dev <bowmans.dev@gmail.com>
2023-04-04 16:35:49 +0100

    onload

--- pages/food.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-const food = document.querySelectorAll('iframe').forEach( item =>
-    console.log(item.contentWindow.document.body.querySelectorAll('iframe'),
-    item.contentWindow.document.body.style = "color: white; font-size: 18px;"
-    )
-  )
-module.exports = food;
